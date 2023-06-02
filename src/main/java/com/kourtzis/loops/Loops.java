cbedf3a20e694ef2ea8ae91571adfaa079ad355a
Athanasios Kourtzis <athanasios.kourtzis@protonmail.com>
2023-06-02 05:57:51 +0200

    Added arraySum method

--- src/main/java/com/kourtzis/loops/Loops.java
+++ src/main/java/com/kourtzis/loops/Loops.java
@@ -2,6 +2,19 @@ package com.kourtzis.loops;
 
 public class Loops {
     public static void main(String[] args) {
-        
+
+    }
+
+    //Aufgabe 2
+    public static double arraySum(double[] arr) {
+        double result = 0;
+        int begin = 0;
+        int end = arr.length;
+        while(begin < end) {
+            result += arr[begin];
+            ++begin;
+        }
+
+        return result;
     }
 }
