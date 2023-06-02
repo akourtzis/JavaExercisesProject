ee933ebb78011a9ab265d32baad60e269155dbaa
Athanasios Kourtzis <athanasios.kourtzis@protonmail.com>
2023-06-02 05:15:53 +0200

    Added revert method

--- src/main/java/com/kourtzis/strings/Strings.java
+++ src/main/java/com/kourtzis/strings/Strings.java
@@ -2,7 +2,18 @@ package com.kourtzis.strings;
 
 public class Strings {
     public static void main(String[] args) {
-        
+
     }
     
+    // Aufgabe 2
+	public static String revert(final String str) {
+		char[] tempStr = str.toCharArray();
+		char[] revStr = new char[tempStr.length];
+		
+		for(int it = revStr.length - 1, index = 0; it >= 0; --it, ++index) {
+			revStr[index] = tempStr[it];
+		}
+		
+		return String.valueOf(revStr);
+	}
 }
