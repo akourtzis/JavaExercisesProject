a35a522d1797e1377f94f2ccc6dd2e541d4c36dd
Athanasios Kourtzis <athanasios.kourtzis@protonmail.com>
2023-06-02 05:18:38 +0200

    Added month as a String functionality
    Added methods for getting a date with the format (dd.mm.yyyy) as a String and returning the month.

--- src/main/java/com/kourtzis/strings/Strings.java
+++ src/main/java/com/kourtzis/strings/Strings.java
@@ -16,4 +16,52 @@ public class Strings {
 		
 		return String.valueOf(revStr);
 	}
+
+    //Aufgabe 3
+	public static String monthAsString(final String str) {
+		if(checkDateFormat(str)) {
+			return getMonth(str);
+		}
+		
+		return "Datum soll im Format \"dd.mm.yyyy\" angegeben werden.";
+	}
+	
+	public static boolean checkDateFormat(final String str) {
+		return str.matches("\\d{2}\\.\\d{2}\\.\\d{4}");
+	}
+	
+	public static String getMonth(final String str) {
+		String[] date = str.split("\\.");
+		String month = date[1];
+		
+		switch(month) {
+		case "01":
+			return "Januar";
+		case "02":
+			return "Februar";
+		case "03":
+			return "Marz";
+		case "04":
+			return "April";
+		case "05":
+			return "Mai";
+		case "06":
+			return "Juni";
+		case "07":
+			return "Juli";
+		case "08":
+			return "August";
+		case "09":
+			return "September";
+		case "10":
+			return "Oktober";
+		case "11":
+			return "November";
+		case "12":
+			return "Dezember";
+		default:
+			return "Unbekannter Monat";
+		
+		}
+	}
 }
