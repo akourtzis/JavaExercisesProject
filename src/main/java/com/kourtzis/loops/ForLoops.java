49283a8610702f0328dacb4402027a6eb3d4675e
Athanasios Kourtzis <athanasios.kourtzis@protonmail.com>
2023-06-07 16:04:40 +0200

    Add sum method

--- src/main/java/com/kourtzis/loops/ForLoops.java
+++ src/main/java/com/kourtzis/loops/ForLoops.java
@@ -5,5 +5,15 @@ public class ForLoops {
 
     }
 
-    
+    public static int Sum(int n) {
+        if(n < 0) {
+            return n;
+        }
+        int sum = 0;
+        for(int it = 0; it < n; ++it) {
+            sum += it;
+        }
+
+        return sum;
+    }
 }
