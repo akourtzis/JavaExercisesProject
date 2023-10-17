312f0e8582fcfa11f6d71f62f20a1eb148960238
Athanasios Kourtzis <athanasios.kourtzis@protonmail.com>
2023-10-17 17:26:11 +0200

    Added Patient class

---
+++ src/main/java/com/kourtzis/oop3/model/Patient.java
@@ -0,0 +1,36 @@
+package com.kourtzis.oop3.model;
+
+import java.util.Date;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+@EqualsAndHashCode(of = { "id"})
+public class Patient {
+    private String id;
+    private String name;
+    private String address;
+    private Date birthday;
+    private String insuranceName;
+    private boolean insuranceCardPresent;
+
+    public Patient(final String id, 
+                   final String name, 
+                   final String address, 
+                   final Date birthday, 
+                   final String insuranceName, 
+                   boolean insuranceCardPresent) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.birthday = birthday;
+        this.insuranceName = insuranceName;
+        this.insuranceCardPresent = insuranceCardPresent;            
+    }
+
+    public Patient(final String id, final String name) {
+        this(id, name, "", null, name, true);
+    }
+}
